d0c96b6a891e0d2f07fe0c8891d107846d4ea540
Nguyen Khac Quang <quangnk@topica.edu.vn>
2015-09-17 15:14:59 +0700

    test new git

---
+++ app.rb
@@ -0,0 +1,28 @@
+require "sinatra"
+require "sinatra/reloader"
+
+$coffees = []
+
+get "/" do
+  template
+end
+
+post "/" do
+  $coffees << params
+  redirect '/'
+end
+
+def template
+  """
+  <html>
+  <body>
+    <form action='/' method='post'>
+      What: <input name='what'>
+      Cost: <input name='cost'>
+      <button type='submit'>add coffee</button>
+    </form>
+    #{ $coffees.inspect }
+  </body>
+  </html>
+  """
+end
